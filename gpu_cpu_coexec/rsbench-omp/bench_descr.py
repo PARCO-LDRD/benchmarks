d121018c96014f2dbafa8f7fea31755fa608b07c
Konstantinos Parasyris <parasyris1@llnl.gov>
2022-03-31 03:38:01 -0700

    rsbench

---
+++ gpu_cpu_coexec/rsbench-omp/bench_descr.py
@@ -0,0 +1,70 @@
+import os
+from bench_modules.benchmark import BaseBenchmark
+import re
+
+
+
+class Benchmark(BaseBenchmark):
+  def __init__(self, system):
+    super().__init__('RSBench')
+    self._root = os.path.split(os.path.realpath(__file__))[0]
+    compile_flags=system.get_compile_flags()
+    self._build = f'FOPENMP="{compile_flags}" make'
+    self._clean = 'make clean'
+    self._inputs = []
+    for size in ['large']:
+      for i in [512, 1024, 2048, 4096, 8192, 16384]:
+        lus = i * 5000
+        in_args = f'-m event -s {size} -l {lus}'
+        self._inputs.append(in_args)
+    self._executable = f'rsbench'
+
+  @property
+  def build(self):
+    return self._build
+
+  @property
+  def clean(self):
+    return self._clean
+
+  @property
+  def inputs(self):
+    return self._inputs
+
+  @property
+  def executable(self):
+    return self._executable
+
+  @property
+  def root(self):
+    return self._root
+
+  def getTime(self, stdout):
+    print(' I am trying t find execution time')
+    exec_time_pattern = '__ExecutionTime__:(.*)'
+    tmp =  re.findall(exec_time_pattern, stdout)
+    if len(tmp) == 0:
+      return None
+    print(tmp, type(tmp))
+    return tmp[0]
+
+  def extractInputFromCMD(self, cmd):
+    vals=cmd.split(' ')
+    return f'{vals[4]}:{vals[6]}'
+
+  def visualize(self, df, outfile, sizes):
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap
+    import seaborn as sns
+    fig, ax = plt.subplots(figsize=sizes)
+    df[['Type', 'lookups']] = df['Input'].str.split(':', expand=True)
+    df['lookups'] = df['lookups'].astype(int)
+    g = sns.relplot(data=df, x='lookups', y='Execution time (s)', col='Type',
+            row='System', hue='Policy', kind='line', facet_kws={'sharey': False,
+                'sharex': True})
+    g.set(xscale="log")
+    g.set(yscale="log")
+    plt.savefig(f'{outfile}')
+    plt.close()
+
