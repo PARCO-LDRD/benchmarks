1ec5133c9339e0c206cd200ab9a7be68705af233
koparasy <parasyris1@llnl.com>
2022-04-01 02:19:42 -0700

    Update viz of amgmk

--- gpu_threads/amgmk-omp/bench_descr.py
+++ gpu_threads/amgmk-omp/bench_descr.py
@@ -51,13 +51,53 @@ class Benchmark(BaseBenchmark):
     from matplotlib.colors import ListedColormap
     import seaborn as sns
     fig, ax = plt.subplots(figsize=sizes)
-    df['Input'] = '$' + df['Input'].str.split(',', expand=True)[0] + '^3$'
-    g = sns.relplot(data=df, x='Input', y='Execution time (s)',
-                    col='System', hue='Policy', kind='line', marker='o')
-    g.set_axis_labels('Grid', 'Execution time (s)\nlog2')
+    print(df)
+    df['Input'] =  df['Input'].str.split(',', expand=True)[0]
+    df['Input'] = df['Input'].astype(int)
+    df = df.rename(columns={'Policy' : 'Num Team Threads'})
+    df['Num Team Threads'] = (df['Num Team Threads'].str.split('_', expand=True))[1].astype(int)
+    oracle = df[df['Execution Type'] == 'Oracle']
+    online = df[df['Execution Type'] == 'Online']
+    print(df)
+    default = df[((df['Num Team Threads'] == 128) & (df['Execution Type'] == 'Static'))]
+    default = default.groupby(['System', 'Input']).mean().reset_index()
+    default['Execution Type'] ='default'
+    online = online.groupby(['Execution Type', 'System', 'Input', 'Num Team Threads']).mean().reset_index()
+    oracle = oracle.groupby(['Execution Type','System', 'Input', 'Num Team Threads']).mean().reset_index()
+    online['Speedup'] = default['Execution time (s)'] / online['Execution time (s)']
+    oracle['Speedup'] = default['Execution time (s)'] / oracle['Execution time (s)']
+    df = df[((df['Execution Type'] == 'Static'))]
+    df = df.groupby(['System', 'Input', 'Num Team Threads']).mean().reset_index()
+    df['Speedup'] = -1.0
+    df = df.set_index(['System', 'Input'])
+    for d in df['Num Team Threads'].unique():
+        print('Current')
+        df.loc[df['Num Team Threads'] == d, 'Speedup'] = df.loc[df['Num Team Threads'] == 128, 'Execution time (s)'] / df.loc[df['Num Team Threads'] == d, 'Execution time (s)']
+        df.loc[df['Num Team Threads'] == d, 'Execution Type'] = f'Static,{d}'
+    print("Online")
+#    df = df[df["Num Team Threads"].isin([64,256,512])]
+    df = pd.concat([online, oracle, df.reset_index()])
+    print(df)
+    g = sns.relplot(data=df, x='Input', 
+                    y='Speedup',
+                    col='System', 
+                    hue='Execution Type', 
+                    style='Num Team Threads',
+                    kind='scatter',
+                    edgecolor='black',
+                    alpha=0.7,
+                    facet_kws={'sharey': False, 'sharex': True}
+                    )
+    for r in g.axes:
+        for c in r:
+            c.axhline(y=1.0, c='gray')
+    g.set_axis_labels('Size', 'Execution time (s)\nlog2')
     g.set_xticklabels(rotation=-90)
-    plt.yscale('log', base=2)
     plt.gca().yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda y, _: '{:.3g}'.format(y)))
     plt.tight_layout()
-    plt.savefig(f'{outfile}')
+    plt.savefig(f'{outfile}_threads_speedup.pdf')
     plt.close()
+    return
+#    plt.tight_layout()
+#    plt.savefig(f'{outfile}')
+#    plt.close()
